3b2faf83cdbfb87f3e9f4636b8c7d00a596bd2f3
Thinley24 <Thinley Jigme>
2023-03-05 11:00:23 +0530

    first commit

---
+++ MyPortfolio/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Fullstack Developer", "Cosmos Enthusiast", "Cyber Geek"],
+    typedSpeed: 100, 
+    backSpeed: 100, 
+    backDelay: 800,
+    loop: true
+})
